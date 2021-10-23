8403e0344db15e4102f5d154a7e309d7632a584b
Rafaela dos Santos <92892141+rafaelam-santos@users.noreply.github.com>
2021-10-23 19:39:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'rafaelam-santos',
+  linkedin: 'rafaela-moreira-dos-santos-bb578b214',
+  facebook: 'rafaela.moreira.1447',
+  instagram: 'rafam_santos',
+  twitter: 'rafaela09622876'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
